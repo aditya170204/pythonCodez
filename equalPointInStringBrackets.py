15203a2b17921700a0cb4685f72865e98a39794e
aditya170204 <adityaraj17feb@gmail.com>
2025-05-06 23:16:34 +0530

    made changes in some code

--- equalPointInStringBrackets.py
+++ equalPointInStringBrackets.py
@@ -14,3 +14,22 @@ def findIndex(str):
                 return i
             
 print(findIndex("(())))("))
+
+
+
+# def findIndex(str):
+        
+#         n=len(str)
+#         opn=[0] * (n + 1)
+#         close= [0] * (n + 1)
+#         for i in range(n):
+#             opn[i + 1] = opn[i] + (1 if str[i] == '(' else 0)
+            
+#         for i in range(n - 1, -1, -1):
+#             close[i] = close[i + 1] + (1 if str[i] == ')' else 0)
+        
+#         for i in range(n + 1):
+#             if opn[i] == close[i]:
+#                 return i
+            
+# print(findIndex("(())))("))
